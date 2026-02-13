9f07128d474aa9d737a199d80698b178f6c19526
Fahadmujtaba98 <fahad.mujtaba@maisonconsulting.com>
2026-02-13 16:14:41 +0500

    fix logs and new files

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', "easypaisa" );
+define( 'DB_NAME', "darazclub" );
 
 /** Database username */
 define( 'DB_USER', "root" );
